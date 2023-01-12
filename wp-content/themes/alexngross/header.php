df29ecfb4f6590f3b54319dab04d9e9753b5f95b
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-01-12 19:21:06 +0100

    hero section

--- wp-content/themes/alexngross/header.php
+++ wp-content/themes/alexngross/header.php
@@ -12,8 +12,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +22,31 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'alexngross' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary">
+			<?php esc_html_e('Skip to content', 'alexngross'); ?>
+		</a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="header">
+			
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$alexngross_description = get_bloginfo( 'description', 'display' );
-			if ( $alexngross_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $alexngross_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'alexngross' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				?>
+			<!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<?php esc_html_e('Primary Menu', 'alexngross'); ?>
+				</button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id' => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
--- wp-content/themes/alexngross/templates/homepage-template.php
+++ wp-content/themes/alexngross/templates/homepage-template.php
@@ -1,5 +1,3 @@
-
-
 <?php
 /*
 Template Name: Homepage
@@ -8,6 +6,54 @@ Template Name: Homepage
 
 get_header();
 ?>
-<h1><?php the_field( 'banner_text' ); ?></h1>
+
+
+<section class='hero-banner'>
+    <?php if (get_field('banner_image')): ?>
+        <img src="<?php the_field('banner_image'); ?>" />
+
+    <?php endif; ?>
+
+    <div class='hero-banner-content'>
+        <h2>
+            <?php the_field('banner_title'); ?>
+        </h2>
+
+        <p>
+            <?php the_field('banner_subtitle'); ?>
+        </p>
+    </div>
+</section>
+
+<section class="container">
+    <div class="content-section">
+        <p>
+            <?php the_field('content_section_text'); ?>
+        </p>
+
+    </div>
+</section>
+
+
+<section class='main-content-section'>
+    <div class='main-content-container'>
+    <h2>
+                <?php the_field('heading_1_title'); ?>
+            </h2>
+        <div class='main-content'>
+            
+           
+                <?php if (get_field('banner_image')): ?>
+
+                    <img src="<?php the_field('heading1_image'); ?>" />
+                <?php endif; ?>
+                <p><?php the_field('heading_1_text'); ?></p>
+       
+        </div>
+    </div>
+</section>
+
+
+
 <?php
 get_footer();
